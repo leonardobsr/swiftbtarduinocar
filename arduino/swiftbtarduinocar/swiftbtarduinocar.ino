bcfbb3bfe926b0f87c68e18c99329e72556a6375
Alexandre Scheer Bing <alexandrebing@gmail.com>
2020-03-05 15:24:48 -0300

    movimentos  pra frente e pra tras por estado

--- arduino/swiftbtarduinocar/swiftbtarduinocar.ino
+++ arduino/swiftbtarduinocar/swiftbtarduinocar.ino
@@ -1,12 +1,19 @@
 
 //PROJETO CARRO EM ARDUINO
 
-int wheel = 7;
-boolean start = false;
+int rightFrontWheel = 7;
+int leftFrontWheel = 6;
+int rightBackWheel = 5;
+int leftBackWheel = 4;
+boolean startFoward = false;
+boolean startBackward = false;
 uint8_t byteRead = 0;
 void setup() {
   //seta a roda na posição 7 como output
-  pinMode(wheel, OUTPUT);
+  pinMode(rightFrontWheel, OUTPUT);
+  pinMode(leftFrontWheel, OUTPUT);
+ pinMode(rightBackWheel, OUTPUT);
+ pinMode(leftBackWheel, OUTPUT);
   //Começa a pegar o input do BT
   Serial.begin(115200);
 }
@@ -18,19 +25,40 @@ void loop() {
     byteRead = Serial.read();
     // Se o comando é A as rodas começam a se mover pra frente
     if( byteRead == 'A' ) {
-      Serial.println("Go foward");
-      start = !start;
+      Serial.println("Go foward pressed");
+      startFoward = !startFoward;
+      startBackward = false;
+    } else if (byteRead == 'B') {
+      Serial.println("Go backward pressed");
+      startBackward = !startBackward;
+      startFoward = false;
     }
     // Demais condições para as outras rodas
-    if(start){
-      digitalWrite(wheel, HIGH);
-      Serial.println("TRUE");
+    if(startFoward){
+      digitalWrite(rightFrontWheel, HIGH);
+       digitalWrite(leftFrontWheel, HIGH);
+       digitalWrite(rightBackWheel, LOW);
+      digitalWrite(leftBackWheel, LOW);
+      Serial.println("Going foward");
+    } else if (startBackward){
+      digitalWrite(rightFrontWheel, LOW);
+       digitalWrite(leftFrontWheel, LOW);
+       digitalWrite(rightBackWheel, HIGH);
+      digitalWrite(leftBackWheel, HIGH);
+      Serial.println("Going backwards");
     } else {
-      digitalWrite(wheel, LOW);
+      digitalWrite(rightFrontWheel, LOW);
+       digitalWrite(leftFrontWheel, LOW);
+       digitalWrite(rightBackWheel, LOW);
+      digitalWrite(leftBackWheel, LOW);
       Serial.println("FALSE");
     }
+    Serial.print("Is going foward? ");
+    Serial.println(startFoward);
+    Serial.print("Is going backward? ");
+    Serial.println(startBackward);
   } else {
-    Serial.println("No command");
+    //Serial.println("No command");
     delay(10);
   }
 }
